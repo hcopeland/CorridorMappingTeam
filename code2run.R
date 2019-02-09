2b98606b35693df0cf3e3997c54c751aa7b84759
Jerod A. Merkle <jmerkle@uwyo.edu>
2019-02-08 21:22:36 -0700

    bigtime update

--- code2run.R
+++ code2run.R
@@ -10,8 +10,8 @@ source("C:/Users/jmerkle/Documents/GitHub/CorridorMappingTeam/functions/create.c
 
 #step 1. Create sequences from the output of Migration Mapper (tab 6)
 create.seqs(shpfl_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6",  #this is the folder where files from tab 6 are located
-             shpfl_name= "pointsOut_20190208080234" ,   
-             migtbl_name="migtime_20190208080234.csv",
+             shpfl_name= "pointsOut_20190208080234" ,  #name of the actual shapefile without the .shp 
+             migtbl_name="migtime_20190208080234.csv",   #name of the actual csv file
              out_fldr="C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/sequences",   #it will make this folder for you
              out_proj="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")   #name a projection you want the output to be in. Then carry this proj through the rest of the steps
 
@@ -21,7 +21,7 @@ create.BBs(seqs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/sequ
                   BBs_out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/UDs",  #it will make this folder for you
                   footprint_out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/Footprints",  #it will make this folder for you
                   metadata_fldr="C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6",
-                  cores=11, location.error=20, cell.size=500, max.lag=8, contour=99,
+                  cores=11, location.error=20, cell.size=1000, max.lag=8, contour=99,
                   proj_of_dbfs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
 #Step 3. Calculate average BBs for each individual and and a population UD and population footprint.
@@ -36,6 +36,7 @@ create.corridors.stopovers(PopUD_asc = "C:/Users/jmerkle/Desktop/Mapp2/Elk_Bench
                            PopFootprint_asc = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/Footprints_pop/popFootprint.asc",
                            pop_BBs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/Footprints_pop",
                            out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/final_products",
-                           stopover_percent=10, corridor_percents=c(10,15,20),min_area = 20000,
+                           stopover_percent=10, corridor_percents=c(10, 20), min_area = 20000,
+                           simplify = TRUE, tolerance = 100, # how to polygons are simplified (unites are meters)
                            proj_of_ascs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
--- functions/create.corridors.stopovers.R
+++ functions/create.corridors.stopovers.R
@@ -2,28 +2,40 @@
 # written by Jerod Merkle, 8 Jan 2019
 # based on code from Hall Sawyer
 
+
+PopUD_asc = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/UDs_pop/averageUD.asc"
+PopFootprint_asc = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/Footprints_pop/popFootprint.asc"
+pop_BBs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/Footprints_pop"
+out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/final_products"
+stopover_percent=10; corridor_percents=c(10, 20);min_area = 20000
+simplify = TRUE; tolerance = 100 # unites are meters
+proj_of_ascs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
+
+
 create.corridors.stopovers <- function(PopUD_asc = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/UDs_pop/averageUD.asc",
                                        PopFootprint_asc = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/Footprints_pop/popFootprint.asc",
                                        pop_BBs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/Footprints_pop",
                                        out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/Elk_BenchCorral_Tab6/final_products",
                                        stopover_percent=10, corridor_percents=c(10, 20),min_area = 20000,
+                                       simplify = TRUE, tolerance = 100, # unites are meters
                                        proj_of_ascs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"){
   
   #manage packages
-  if(all(c("raster","move","igraph","rgeos") %in% installed.packages()[,1])==FALSE)
+  if(all(c("raster","move","igraph","rgeos","maptools") %in% installed.packages()[,1])==FALSE)
     stop("You must install the following packages: raster,igraph, rgeos, move")
   require(raster)
   require(move)
   require(igraph)
   require(rgeos)
-
+  require(maptools)
+  
   #check the new directories
   if(dir.exists(out_fldr)==FALSE){
     dir.create(out_fldr)
   }
   if(length(dir(out_fldr))> 0)
     stop("Your out_fldr Has something in it. It should be empty!")
-
+  
   popUD <- raster(PopUD_asc)
   projection(popUD) <- proj_of_ascs
   
@@ -43,34 +55,129 @@ create.corridors.stopovers <- function(PopUD_asc = "C:/Users/jmerkle/Desktop/Map
   
   #convert final raster to polygons
   stopovers <- rasterToPolygons(stopovers, dissolve=TRUE)
+  if(simplify==TRUE){
+    stopovers<- thinnedSpatialPoly(stopovers, tolerance = tolerance, topologyPreserve=TRUE)
+  }
   writeOGR(stopovers, out_fldr, "stopovers", driver="ESRI Shapefile")
   
-  # now for corridors
+  #-------------------#
+  # now for corridors #
+  #-------------------#
   popFootprint <- raster(PopFootprint_asc)
   projection(popFootprint) <- proj_of_ascs
-  
-  # for the low use corridors
-  lowusecorridor <- reclassify(popFootprint, rcl=matrix(c(0,0.000001,0,0.000001,Inf,1),2,3, byrow=T))
-  lowusecorridor[values(lowusecorridor)==0] <- NA
-  lowusecorridor <- rasterToPolygons(lowusecorridor, dissolve=TRUE)
-  writeOGR(lowusecorridor, out_fldr, "corridor_lowuse1ormore", driver="ESRI Shapefile")
-  
-  lowuse2ormore <- reclassify(popFootprint, rcl=matrix(c(0,1.9,0,1.9,Inf,1),2,3, byrow=T))
-  lowuse2ormore [values(lowuse2ormore)==0] <- NA
-  lowuse2ormore <- rasterToPolygons(lowuse2ormore, dissolve=TRUE)
-  writeOGR(lowuse2ormore, out_fldr, "corridor_lowuse2ormore", driver="ESRI Shapefile")
   
   # for the other corridors based on % of individuals collared
   numb_ids <- length(list.files(pop_BBs_fldr, pattern=".asc"))-1
   print(paste("The number of ids in your populatoin is ", numb_ids, ". If that doesn't look right, then do some investigation.", sep=""))
+  
   popFootprint <- popFootprint/numb_ids
   
-  for(i in 1:length(corridor_percents)){
-    medusecorridor <- reclassify(popFootprint, rcl=matrix(c(0,corridor_percents[i]/100,0,corridor_percents[i]/100,1,1),2,3, byrow=T))
-    medusecorridor[values(medusecorridor)==0] <- NA
-    medusecorridor <- rasterToPolygons(medusecorridor, dissolve=TRUE)
-    writeOGR(medusecorridor, out_fldr, paste("corridor_percent",corridor_percents[i],sep=""), driver="ESRI Shapefile")
+  corridor_percents <- corridor_percents[corridor_percents/100 > 2/numb_ids]
+  
+  thresholdQuantiles = c(0, c(2/numb_ids)-0.0001, (corridor_percents/100)-0.0001, .99)  #this starts with low 1 or more, then low 2 or more, then the other percents
+  thresholdQuantiles_names <- c(1,2,corridor_percents)
+  
+  # compute the contours
+  classifiedRaster = cut(popFootprint,breaks=thresholdQuantiles)
+  
+  # extract the contours as polygons
+  polygonList = rasterToPolygons(classifiedRaster,dissolve=T)
+  
+  # Methods - Data Munging -------------------------------------------
+  # reads and updates the uniqueID global variable to assign 
+  # identifiers to each polygon (unique across shapefiles).
+  makeIndividualPolygons<-function(multiPoly,attr,minArea=0){
+    #polygon.list = polygonList.acc[[1]]@polygons[[1]]@Polygons
+    polygon.list = multiPoly@Polygons
+    area = sapply(polygon.list,function(x) x@area)
+    isHole = sapply(polygon.list,function(x) x@hole)
+    area[which(isHole)]=-1*area[which(isHole)]
+    
+    holeIndices = which(!isHole)
+    
+    diffArea=numeric()
+    startID=uniqueID+1
+    if(length(holeIndices)>1){
+      polygon.list.wrap = lapply(1:(length(holeIndices)-1),function(x) {
+        uniqueID<<-uniqueID+1
+        temp = holeIndices[x]:(holeIndices[x+1]-1)
+        diffArea[x]<<-sum(area[temp])
+        Polygons(polygon.list[temp],uniqueID)
+      })
+    }else{
+      polygon.list.wrap = list()
+    }
+    
+    uniqueID<<-uniqueID+1
+    temp = holeIndices[length(holeIndices)]:length(polygon.list)
+    polygon.list.wrap[[length(polygon.list.wrap)+1]] = Polygons(polygon.list[temp],uniqueID)
+    diffArea[length(holeIndices)]=sum(area[temp])
+    endID=uniqueID
+    id=startID:endID
+    
+    temp = which(diffArea>minArea)
+    diffArea=diffArea[temp]
+    polygon.list.wrap=polygon.list.wrap[temp]  
+    id=id[temp]
+    
+    #sum(sapply(1:length(polygon.list.wrap), function(x) sapply(polygon.list.wrap[[x]]@Polygons, function(y) y@hole)))
+    #sapply(polygon.list.wrap[[2]]@Polygons,function(x) x@hole)
+    #print(sapply(1:length(index), function(x) sapply(polygon.list.wrap[[x]]@Polygons, function(y) y@hole=isHole[x])))
+    
+    attrTable = data.frame(ID=id, GRIDCODE=attr)
+    row.names(attrTable) = id
+    
+    x=SpatialPolygonsDataFrame(SpatialPolygons(polygon.list.wrap), data=attrTable)
+    
+    #print(length(sapply(sapply(x@polygons,function(y) sapply(y@Polygons, function(z) z@hole)),sum)))
+    #print(sum(sapply(sapply(x@polygons,function(y) sapply(y@Polygons, function(z) z@hole)),sum)))
+    x
   }
+  
+  
+  
+  # NOTE: The simplification is set to preserve topology; although not 
+  # strictly necessary for visualizaiton, this avoids the scenario where
+  # large polygons were mysteriously getting excluded from the output.
+  # each polygon contains only the area within a single cut
+  # join the polygons with any in higher cuts (cumulative area for each level)
+  n=length(polygonList@polygons)
+  
+  polygonList.acc <- lapply(1:n, function(x){
+    idList = c(rep(NA,x-1),rep(1,n-x+1))
+    #print(idList)
+    unionSpatialPolygons(SpatialPolygons(polygonList@polygons),idList)
+  })
+  
+  # line simplification
+  if(simplify==TRUE){
+    polygonList.acc <- lapply(polygonList.acc,thinnedSpatialPoly,
+                              tolerance,topologyPreserve=T)
+  }
+  
+  # split up multi-polygons into separate polygons
+  uniqueID<-0 # reset before calling makeIndividual polygons
+  spDataFrame.list <- sapply(1:length(polygonList.acc),function(x) makeIndividualPolygons(
+    polygonList.acc[[x]]@polygons[[1]],thresholdQuantiles_names[x],min_area))
+  #sapply((sapply(spDataFrame.list,function(x) sapply(x@polygons,function(y) sapply(y@Polygons, function(z) z@hole)))),sum)
+  
+  for(x in 1:length(spDataFrame.list)){
+    proj4string(spDataFrame.list[[x]])<-proj_of_ascs
+  }
+  
+  bigData.df = data.frame(ID=numeric(), GRIDCODE=numeric())
+  bigData.sp = list()
+  # accumulate the data
+  for(i in 1:length(spDataFrame.list)){
+    bigData.df = rbind(bigData.df,spDataFrame.list[[i]]@data)
+    bigData.sp = append(bigData.sp,spDataFrame.list[[i]]@polygons)
+  }
+  # create the spatial data frame with a projection (same as above)
+  bigData = SpatialPolygonsDataFrame(SpatialPolygons(bigData.sp),bigData.df)
+  proj4string(bigData) <- proj_of_ascs
+  # write to a shapefile
+  writeOGR(bigData,out_fldr,"corridors", driver="ESRI Shapefile")
+  
   print("Success. Check your folders")
   return("Done.")
 }
