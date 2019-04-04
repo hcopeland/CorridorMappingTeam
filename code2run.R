445419f02320eff5d1ece2018b6ba63d4eb7177d
Jerod A. Merkle <jmerkle@uwyo.edu>
2019-04-04 17:47:33 -0600

    a few updates

--- code2run.R
+++ code2run.R
@@ -30,7 +30,7 @@ create.BBs(seqs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/sequences",   
                   BBs_out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/UDs",  #it will make this folder for you
                   footprint_out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/Footprints",  #it will make this folder for you
                   metadata_fldr="C:/Users/jmerkle/Desktop/Mapp2/tab6output",
-                  cores=11, location.error=20, cell.size=1000, max.lag=8, contour=99, time.step=10,
+                  cores=11, location.error=20, cell.size=50, max.lag=8, contour=99, time.step=5,
                   proj_of_dbfs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
 #Step 3. Calculate average BBs for each individual and and a population UD and population footprint.
@@ -46,8 +46,8 @@ create.corridors.stopovers(PopUD_asc = "C:/Users/jmerkle/Desktop/Mapp2/tab6outpu
                            PopFootprint_asc = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/Footprints_pop/popFootprint.asc",
                            pop_BBs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/Footprints_pop",
                            out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/final_products",
-                           stopover_percent=10, corridor_percents=c(10, 20), min_area = 20000,
-                           simplify = TRUE, tolerance = 50, # how to polygons are simplified (unites are meters)
+                           stopover_percent=10, corridor_percents=c(10, 20), min_area = 20000, #this is in squared meters
+                           simplify = TRUE, tolerance = 25, # how to polygons are simplified (unites are meters)
                            proj_of_ascs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
 #--------------------------#
@@ -80,7 +80,7 @@ create.BBs.W(seqs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/sequencesW",
              BBs_out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/UDsW",  #it will make this folder for you
              metadata_fldr="C:/Users/jmerkle/Desktop/Mapp2/tab6output",
              mindays=30,   #if an individual animal has less than this many days in a given sequence of winter data, it will be removed
-             cores=11, location.error=20, cell.size=1000, max.lag=8, time.step=10,
+             cores=11, location.error=20, cell.size=50, max.lag=8, time.step=5,
              proj_of_dbfs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
 #Step 3. Calculate average BBs for each individual and and a population UD.
@@ -92,8 +92,8 @@ create.BB.avgs.W(BBs_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/UDsW",   
 # Step 4. Calculate the winter contours
 create.core.areas.W(PopUD_asc = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/UDs_popW/averageUD_winter.asc",
                     out_fldr = "C:/Users/jmerkle/Desktop/Mapp2/tab6output/final_productsW",
-                    core_contours=c(10,20,30,40,50,60,70,80,90,99), min_area = 20000,
-                    simplify = TRUE, tolerance = 100, # unites are meters
+                    core_contours=c(10,20,30,40,50,60,70,80,90,99), min_area = 20000,  #this is in squared meters
+                    simplify = TRUE, tolerance = 25, # unites are meters
                     proj_of_ascs="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
 
