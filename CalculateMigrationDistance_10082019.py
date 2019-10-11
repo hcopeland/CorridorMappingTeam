d9c78fe80fdb3ca529b51f7a6cd872c14b5938a4
Holly Copeland <7328603+hcopeland@users.noreply.github.com>
2019-10-11 10:44:32 -0600

    Update CalculateMigrationDistance_10082019.py

--- CalculateMigrationDistance_10082019.py
+++ CalculateMigrationDistance_10082019.py
@@ -10,18 +10,20 @@ arcpy.env.overwriteOutput = True
 import pandas
 
 # Set environment settings
-#Location of line file - CHANGE THIS DIRECTORY to where your data is!!!
+#Location of line file and local variables - CHANGE THIS DIRECTORY to where your data is!!!
 arcpy.env.workspace = "C:/Holly/Work/Projects/EasternYellowstoneMuleDeer2016/Data/CMT_Dubois_8262019/migration_lines/"
 
-# Replace a layer/table view name with a path to a dataset (which can be a layer file) or create the layer/table view within the script
-# The following inputs are layers or table views: "migration_lines"
-arcpy.SimplifyLine_cartography(in_features="migration_lines.shp", out_feature_class="migration_lines_simplify.shp",
-                               algorithm="POINT_REMOVE", tolerance="500 Meters",
-                               error_resolving_option="RESOLVE_ERRORS", collapsed_point_option="NO_KEEP",
-                               error_checking_option="CHECK", in_barriers="")
+#Execute GeneratePointsAlongLines by percentage and convert back to lines
+in_features = 'migration_lines.shp'
+arcpy.GeneratePointsAlongLines_management(in_features, 'migration_points_simplify.shp', 'PERCENTAGE',
+                                          Percentage=5, Include_End_Points='END_POINTS')
 
+arcpy.PointsToLine_management('migration_points_simplify.shp', 'migration_lines_simplify.shp', 'mig')
+
+#add area field
 arcpy.AddField_management('migration_lines_simplify.shp','MILES','DOUBLE')
 arcpy.CalculateField_management("migration_lines_simplify.shp",'MILES','!shape.length@miles!','PYTHON_9.3')
+
 
 #Statistics to get mean, max, min
 out_table = r"in_memory\stats_table"
@@ -40,5 +42,6 @@ df = pandas.DataFrame(data=[row for row in cursor],
                       columns=field_names)
 
 print(df)
+print("Check your output in ArcGIS to be sure you have captured the longest migration!")
 
 #    print ("Max migration length = {} miles".format(max(cursor)))
